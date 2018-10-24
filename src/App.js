6c2b2fcecdb2e5dc293c20ed2a76c41eac00d47d
RyderBlack <ryhad.boughanmi@gmail.com>
2018-10-24 15:06:34 -0400

    form comp added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Form from './components/Form'
 
 class App extends Component {
   render() {
@@ -8,19 +9,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Inventis</h1>
         </header>
+        <Form />
       </div>
+      
     );
   }
 }
---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Form = () => (
+    <form>
+        <label>
+        Name:
+        <input type="text" name="name" />
+        </label>
+        <input type="submit" value="Submit" />
+    </form>
+);
+
+export default Form;
