aa29e4094ac68e3cbb6bc442be05ec75cec0ac0a
RyderBlack <ryhad.boughanmi@gmail.com>
2018-10-24 15:28:45 -0400

    adding inputs

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,9 +3,30 @@ import React from 'react';
 const Form = () => (
     <form>
         <label>
-        Name:
+        Nom:
         <input type="text" name="name" />
         </label>
+        <br />
+        
+        <label>
+        Reference:
+        <input type="text" name="reference" />
+        </label>
+        <br />
+
+        <label>
+        Nom:
+        <input type="text" name="name" />
+        </label>
+
+        <div>
+        <label for="pic">Photo:</label>
+        <input type="file"
+               id="pic" name="pic"
+               accept="image/png, image/jpeg" />
+        </div>
+
+        <hr />
         <input type="submit" value="Submit" />
     </form>
 );
